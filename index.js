0e204a36f293926e24255e18f5fdcf4f6121c6e4
matheusgabg <matheusgabg@gmail.com>
2020-03-10 12:08:23 -0400

    2 files modified

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+
+import Game from './components/game';
 import './style.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      name: 'React'
-    };
   }
 
   render() {
     return (
-      <div>aaaaaaaaa</div>
+      <Game />
     );
   }
 }
